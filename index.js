34387f300eb9a641340d87f8c05589fbacf306e0
Kona <landon.deroin@gmail.com>
2023-07-24 18:24:03 -0400

    added license badge

--- index.js
+++ index.js
@@ -1,78 +1,69 @@
 // TODO: Include packages needed for this application
-const fs = require('fs');
-const inquirer = require('inquirer');
-const generateMarkdown = require("./utils/generateMarkdown")
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions =  [
-{
-    type: 'input',
-    name: 'title',
-    message: "What do we call your project?"
-},
-{
-    type: 'input',
-    name: 'description',
-    message: "Please enter a small description"
-},
-{
-    type: 'input',
-    name: 'instal',
-    message: "What do you need to install?"
-},
-{
-    type: 'input',
-    name: 'usage',
-    message: "How to use"
-},
-{
-    type: 'list',
-    name: 'license',
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What do we call your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please enter a small description",
+  },
+  {
+    type: "input",
+    name: "instal",
+    message: "What do you need to install?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "How to use",
+  },
+  {
+    type: "list",
+    name: "license",
     message: "what license?",
-    choices: ['MIT', 'Apache', 'Other', 'None']
-},
-{
-    type: 'input',
-    name: 'contribute',
-    message: "Who Worked on it"
-},
-{
-    type: 'input',
-    name: 'test',
-    message: "What command will be used for running tests"
-},
-{
-    type: 'input',
-    name: 'username',
-    message: "What is your github username"
-},
-{
-    type: 'input',
-    name: 'email',
-    message: "what is your email address"
-}
-
-
-
-
-
-
-
+    choices: ["MIT", "Apache", "Other", "None"],
+  },
+  {
+    type: "input",
+    name: "contribute",
+    message: "Who Worked on it",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "What command will be used for running tests",
+  },
+  {
+    type: "input",
+    name: "username",
+    message: "What is your github username",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "what is your email address",
+  },
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) =>
+  fs.writeFile(fileName, data, (err) =>
     err ? console.error(err) : console.log("Success!")
-    )
-    
+  );
 }
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions)
-    .then (function(answers){
-        writeToFile('read.md', generateMarkdown(answers));
-    })
+  inquirer.prompt(questions).then(function (answers) {
+    writeToFile("README.md", generateMarkdown(answers));
+  });
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,15 +1,16 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let badge = "";
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
+  if (license !== "None") {
+    badge =
+      "![Badge for License](https://shields.io/badge/license-" +
+      license +
+      "-purple.svg)";
+  }
+  return badge;
+}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
