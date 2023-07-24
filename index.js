e7afa86dcfce296ceac155065b92fe30387d8e30
Kona <landon.deroin@gmail.com>
2023-07-24 18:01:03 -0400

    added inputs to index.js

--- index.js
+++ index.js
@@ -1,11 +1,65 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generateMarkdown = require("./utils/generateMarkdown")
 // TODO: Create an array of questions for user input
-const questions = ['title?', 'what is the description', 'instalation instructions', 'how to use it?', 'what license?', ''];
+const questions =  inquirer.prompt([
+{
+    type: 'input',
+    name: 'title',
+    message: "What do we call your project?"
+}
+{
+    type: 'input',
+    name: 'description',
+    message: "Please enter a small description"
+}
+{
+    type: 'input',
+    name: 'instal',
+    message: "What do you need to install?"
+}
+{
+    type: 'input',
+    name: 'usage',
+    message: "How to use"
+}
+{
+    type: 'list',
+    name: 'license',
+    message: ['MIT', 'Apache', 'Other', 'None']
+}
+{
+    type: 'input',
+    name: 'contribute',
+    message: "Who Worked on it"
+}
+{
+    type: 'input',
+    name: 'test',
+    message: "What command will be used for running tests"
+}
+{
+    type: 'input',
+    name: 'username'
+    message: "What is your github username"
+}
+{
+    type: 'input',
+    name: 'email',
+    message: "what is your email address"
+}
+
+
+
+
+
+
+
+]);
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
+function writeToFile(data) {
 
     
 }
