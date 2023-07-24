ce234da594e956bf547fa9aecdcac2a6071d5bd3
Kona <landon.deroin@gmail.com>
2023-07-24 18:11:07 -0400

    wrote write to file in index

--- index.js
+++ index.js
@@ -61,7 +61,9 @@ const questions =  [
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-
+    fs.writeFile(fileName, data, (err) =>
+    err ? console.error(err) : console.log("Success!")
+    )
     
 }
 
