23bf22493da4448d3022394e1f78e46d15609619
Kona <landon.deroin@gmail.com>
2023-07-24 18:08:46 -0400

    init function in index

--- index.js
+++ index.js
@@ -3,47 +3,48 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require("./utils/generateMarkdown")
 // TODO: Create an array of questions for user input
-const questions =  inquirer.prompt([
+const questions =  [
 {
     type: 'input',
     name: 'title',
     message: "What do we call your project?"
-}
+},
 {
     type: 'input',
     name: 'description',
     message: "Please enter a small description"
-}
+},
 {
     type: 'input',
     name: 'instal',
     message: "What do you need to install?"
-}
+},
 {
     type: 'input',
     name: 'usage',
     message: "How to use"
-}
+},
 {
     type: 'list',
     name: 'license',
-    message: ['MIT', 'Apache', 'Other', 'None']
-}
+    message: "what license?",
+    choices: ['MIT', 'Apache', 'Other', 'None']
+},
 {
     type: 'input',
     name: 'contribute',
     message: "Who Worked on it"
-}
+},
 {
     type: 'input',
     name: 'test',
     message: "What command will be used for running tests"
-}
+},
 {
     type: 'input',
-    name: 'username'
+    name: 'username',
     message: "What is your github username"
-}
+},
 {
     type: 'input',
     name: 'email',
@@ -56,16 +57,21 @@ const questions =  inquirer.prompt([
 
 
 
-]);
+];
 
 // TODO: Create a function to write README file
-function writeToFile(data) {
+function writeToFile(fileName, data) {
 
     
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then (function(answers){
+        writeToFile('read.md', generateMarkdown(answers));
+    })
+}
 
 // Function call to initialize app
 init();
